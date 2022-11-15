602bf63146bf02e4483e5567db1cddc8702f74fd
Ersoylu <emreersoylu@hotmail.com>
2022-11-15 19:02:01 +0100

    JWT infrastructure , Login and Register operations.

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -9,5 +9,11 @@ namespace Business.Constants
 	public class Messages
 	{
 		public static string AddedCompany = "Registration of the company is succesfull";
+		public static string UserNotFound = "User not found";
+		public static string PasswordError = "Password is wrong";
+		public static string SuccessfulLogin = "Login is successful";
+		public static string UserRegistered = "Registration of user is successful";
+		public static string UserAlreadyExits = "User allready Exits";
+
 	}
 }
